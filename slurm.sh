26fc4666ab2c108b1275cc10b6728b0d3a885f33
acrucha <mcaa@cin.ufpe.br>
2024-09-30 19:59:31 -0300

    feat: add slurm bash

---
+++ slurm.sh
@@ -0,0 +1,17 @@
+# !/bin/bash
+# SBATCH --job-name=vss-attacker-ddpg
+# SBATCH --ntasks=1
+# SBATCH --mem 32G
+# SBATCH -c 16
+# SBATCH -o vss-attacker-ddpg.log
+# SBATCH --output=vss-attacker-ddpg-output.txt
+# SBATCH --error=vss-attacker-ddpg-error.txt
+
+module load Python/3.10
+
+source $HOME/vss-rl-agents/bin/activate
+
+python $HOME/robocin/vss-rl-agents/train_ddpg.py --env "Pid-v0" --name "PIDTuning-DDPG"
+python $HOME/robocin/vss-rl-agents/train_ddpg.py --env "Penalty-v0" --name "Penalty-DDPG"
+python $HOME/robocin/vss-rl-agents/train_ddpg.py --env "Attacker-v0" --name "VSSEF-DDPG"
+python $HOME/robocin/vss-rl-agents/train_ddpg.py --env "VSS-v0" --name "VSS-DDPG"
